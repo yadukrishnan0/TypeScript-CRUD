bcac16de1b23559fcf62a0bfa313181097d915b3
yadukrishnan <yadhu4507@gmail.com>
2024-06-21 20:03:28 +0530

    product edit success

--- controllers/productController.ts
+++ controllers/productController.ts
@@ -1,5 +1,6 @@
 import { Response, Request } from "express";
 import { productModel } from "../models/productSchema";
+
 export default {
   addProduct: async (req: Request, res: Response) => {
     try {
@@ -17,4 +18,23 @@ export default {
       return res.status(500).send("internal server error");
     }
   },
+  
+  editProduct: async (req: Request, res: Response) => {
+    try {
+      const id = req.query.id as string;
+      const { productName, Price, Discount, color, size } = req.body;
+      await productModel.updateOne({ _id: id }, {
+        $set: {
+          productName,
+          Price,
+          Discount,
+          color,
+          size
+        }
+      });
+      res.status(200).send("product edit success");
+    } catch (err) {
+      res.status(500).send("internal server error");
+    }
+  }
 };
--- routers/productRouter.ts
+++ routers/productRouter.ts
@@ -3,6 +3,6 @@ import productController from "../controllers/productController";
 import { Router } from "express";
 const ProductRouter = Router()
 ProductRouter.post('/addproduct',productController.addProduct);
-
+ProductRouter.post('/editproduct',productController.editProduct)
 export default ProductRouter;
 
